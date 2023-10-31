b019d795f4b8963cb7324f5f5e45a198bb4eba0f
Nate Brevin Que <nate.que@obf.ateneo.edu>
2023-10-31 09:46:55 +0800

    Fixed health bar and added color indications

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -11,6 +11,10 @@ public class HealthBar : MonoBehaviour
     private float y;
     private float width;
 
+    private float offset;
+
+    private SpriteRenderer sr;
+
     void Start()
     {
         Player = (PlayerController) FindObjectOfType(typeof(PlayerController));
@@ -19,19 +23,30 @@ public class HealthBar : MonoBehaviour
         x = objTransform.position.x;
         y = objTransform.position.y;
         width = 1;
+
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        x = Player.getX();
+        x = Player.getX() - offset;
         y = Player.getY() - 0.65f;
 
         objTransform.localScale = new Vector3(width, objTransform.localScale.y, 0);
         objTransform.position = new Vector3(x, y, 0);
+
+        if (width <= 0.25f){
+            sr.color = new Color(1, 0.2706f, 0);
+        } else if (width <= 0.5f){
+            sr.color = Color.yellow;
+        } else if (width <= 0.25f){
+            sr.color = Color.cyan;
+        }
     }
 
     public void Configure()
     {
         width -= 0.05f;
+        offset = 1.5f * (1 - width) / 2;
     }
 }
